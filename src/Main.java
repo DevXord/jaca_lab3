2866496b137de498099e1a71acd68809a0198c86
DevXord <xordii.xd@gmail.com>
2021-11-29 17:48:41 +0100

    ĆW_1 COMMIT

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Main {
+
+
+
 
     public static void main(String[] args) {
 
@@ -16,9 +20,49 @@ public class Main {
         do podanych cyfr przez użytkownika,
         g) gdy użytkownik wciśnie 0 ma wyświetlić się status KONIEC w innym wypadku KONTYNUUJEMY :),
         */
+        Scanner sc = new Scanner(System.in);
 
+        while (true){
+
+            System.out.print("Wpisz liczbę od 1 do 6\n ");
+            System.out.print("0 - Wyjdź\n");
+
+            System.out.print("Co chcesz zrobić?\n>> ");
+            int input = sc.nextInt();
+
+            switch (input) {
+                case 1 -> System.out.print("\n"+(LiczbyEnum.Jeden) + "\n");
+                case 2 -> System.out.print("\n"+(LiczbyEnum.Dwa) + "\n");
+                case 3 -> System.out.print("\n"+(LiczbyEnum.Trzy) + "\n");
+                case 4 -> System.out.print("\n"+(LiczbyEnum.Cztery) + "\n");
+                case 5 -> System.out.print("\n"+(LiczbyEnum.Piec) + "\n");
+                case 6 -> System.out.print("\n"+(LiczbyEnum.Szesc) + "\n");
+                default -> {
+                    if(input != 0){ System.out.print("\nBłędna wartość\n");}
+                }
+            }
+
+            if(input == 0) {
+                System.out.print("\n"+StatusEnum.KONIEC +"\n\n");
+                break;
+            }
+            else
+            {
+                System.out.print("\n"+StatusEnum.KONTYNUUJEMY +" :)\n\n");
+            }
+
+        }
         /* statusy i wybór ze switch case to najczęstsze użycia enumów, enumy są często używane
         * w różnego rodzaju listach, selectach jako krótkie opcje wyboru np. jako status A, X (Aktualny, Usunięty) */
 
     }
+
+}
+
+enum LiczbyEnum {
+    Jeden, Dwa, Trzy, Cztery, Piec, Szesc;
+}
+
+enum StatusEnum {
+    KONTYNUUJEMY, KONIEC;
 }
